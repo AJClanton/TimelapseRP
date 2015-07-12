ceda330c483444754d9d2f0da7ba094e84a3ea61
Ervin <ervin@xtb.me>
2015-07-12 11:48:30 -0500

    Added Maven Repo

---
+++ src/main/java/com/timelapserp/TimeLapseRP.java
@@ -0,0 +1,17 @@
+package com.timelapserp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Ervin on 7/12/2015.
+ */
+public class TimeLapseRP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
--- src/main/java/timelapserp/TimeLapseRP.java
+++
@@ -1,7 +0,0 @@
-package main.java.timelapserp;
-
-/**
- * Created by Ervin on 7/12/2015.
- */
-public class TimeLapseRP {
-}
